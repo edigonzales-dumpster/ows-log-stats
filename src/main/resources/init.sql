282921e6e535da1007e7e0b3af289d71d4146e08
edigonzales <edi.gonzales@gmail.com>
2021-05-31 20:16:39 +0200

    wip

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -15,3 +15,17 @@ CREATE TABLE wms_request (
 );
 
 CREATE INDEX IF NOT EXISTS request_time_idx ON wms_request (request_time);
+CREATE INDEX IF NOT EXISTS wms_request_type_idx ON wms_request (wms_request_type);
+CREATE INDEX IF NOT EXISTS wms_srs_idx ON wms_request (wms_srs);
+CREATE INDEX IF NOT EXISTS wms_width_idx ON wms_request (wms_width);
+CREATE INDEX IF NOT EXISTS wms_height_idx ON wms_request (wms_height);
+
+CREATE TABLE wms_request_layer ( 
+   id IDENTITY NOT NULL PRIMARY KEY,
+   request_id INTEGER,
+   layer VARCHAR(255),
+   foreign key (request_id) references wms_request(id)
+);
+
+CREATE INDEX IF NOT EXISTS request_id_idx ON wms_request_layer (request_id);
+CREATE INDEX IF NOT EXISTS layer_idx ON wms_request_layer (layer);
